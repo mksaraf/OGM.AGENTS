ab544c7a29787868e657062d0c64618f1c3e1fe4
Manish Kumar Saraf <mksaraf@outlook.com>
2025-02-22 14:34:53 -0600

    SRC/APP ADDED

--- src/app/app.py
+++ src/app/app.py
@@ -1,5 +1,8 @@
 #### Import ####
+__import__('pysqlite3')
 import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 module_path = 'src/app/'
 # Add the module directory to sys.path
 sys.path.append(module_path)
